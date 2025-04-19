3d02091c3e9b885c31ef4fed5de0f5fda70cf807
MultiClone Developer <multiclone@example.com>
2025-04-19 22:03:51 +0000

    Initial commit: Project configuration files

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.20")
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.0")
     }
 }
 
